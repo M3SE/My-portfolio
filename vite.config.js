3ea76eb16bbf02946f6fc2825080c87bb9780d65
M3SE <simonc691@gmail.com>
2024-12-04 12:35:00 -0500

    Changed base in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfolio/",
+  base: "/My-portfolio/",
   css: {
     postcss: './postcss.config.js',
   },
